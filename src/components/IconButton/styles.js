497f1c332f3f3aa7bdfd3aedf59ea711227c885b
Dotography-Weerapat <weerapat.t@scale360solutions.com>
2018-03-15 18:29:45 +0700

    fix circle

--- src/components/IconButton/styles.js
+++ src/components/IconButton/styles.js
@@ -3,18 +3,18 @@ import { GlobalStyle } from '../../styles'
 import { Theme } from '../../config/theme'
 
 class Style extends GlobalStyle {
-  btnView = (type, color, size) => ({
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: type === 'outline' || type === 'flat' ? 'transparent' : _.get(Theme.Button.Color, color, color),
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderColor: _.get(Theme.Button.Text, color, color),
-    borderWidth: type === 'outline' ? 1 : 0,
-    width: size,
-    height: size,
-    borderRadius: size / 2
-  })
+  btnView(type, color, size) {
+    return {
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: type === 'outline' || type === 'flat' ? 'transparent' : _.get(Theme.Button.Color, color, color),
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      borderColor: _.get(Theme.Button.Text, color, color),
+      borderWidth: type === 'outline' ? 1 : 0,
+      ...this.circle(size),
+    }
+  }
 }
 
 export default new Style()
