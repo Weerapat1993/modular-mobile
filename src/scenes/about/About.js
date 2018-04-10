43fa9c1c6a04003f3be77eab6b495c603157cc80
Top <weerapat.thawatchoketawee@gmail.com>
2018-04-10 23:02:23 +0700

    add case form & state

--- src/scenes/about/About.js
+++ src/scenes/about/About.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { object } from 'prop-types'
-import { ScrollView } from 'react-native'
+import { ScrollView, Text } from 'react-native'
+import { List, InputItem, Toast } from 'antd-mobile'
+import { createForm } from 'rc-form';
 import { bindActionCreators } from 'redux'
 import { localStorage } from '../../features/storage/redux/storageActions'
 import { PurchaseSelector as Selector } from '../../features'
@@ -15,14 +17,49 @@ class About extends Component {
     localStorage: object.isRequired,
   }
 
+  constructor() {
+    super()
+
+    this.state = {
+      boxSwitch: false,
+      hasError: false,
+      value: '',
+    }
+
+    this.handleErrorClick = this.handleErrorClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
   shouldComponentUpdate(nextProps, nextState) {
-    if(Performances.isShouldRenderProps(this.props, nextProps)) return false
-    if(Performances.isShouldRenderState(this.state, nextState)) return false
+    const checkProps = Performances.isShouldRenderProps(this.props, nextProps)
+    const checkState = Performances.isShouldRenderState(this.state, nextState)
+    if(checkProps && checkState) return false
     return true
+  }
+
+  handleErrorClick() {
+    if (this.state.hasError) {
+      Toast.info('Please enter 11 digits');
+    }
+  }
+  handleChange(value) {
+    if (value.replace(/\s/g, '').length < 11) {
+      this.setState({
+        hasError: true,
+      });
+    } else {
+      this.setState({
+        hasError: false,
+      });
+    }
+    this.setState({
+      value,
+    });
   }
 
   render() {
     const { localStorage } = this.props
+    const { boxSwitch } = this.state
     alert('Render')
     return (
       <ScrollView>
@@ -32,8 +69,35 @@ class About extends Component {
           rounded 
           style={styles.marginVertical(10)} 
           onPress={() => localStorage.setItem('socialList', faker.name.findName())}>
-          Render
+          Render Props
         </Button>
+        <Button 
+          type='primary' 
+          color='default' 
+          rounded 
+          style={styles.marginVertical(10)} 
+          onPress={() => this.setState({ boxSwitch: false })}>
+          Render State = false
+        </Button>
+        <Button 
+          type='primary' 
+          color='default' 
+          rounded 
+          style={styles.marginVertical(10)} 
+          onPress={() => this.setState({ boxSwitch: !boxSwitch })}>
+          Switch State
+        </Button>
+        <Text>Switch: {JSON.stringify(boxSwitch)}</Text>
+        <List>
+          <InputItem
+            type="phone"
+            placeholder="input your phone"
+            error={this.state.hasError}
+            onErrorClick={this.handleErrorClick}
+            onChange={this.handleChange}
+            value={this.state.value}
+          />
+        </List>
       </ScrollView>
     )
   }
@@ -47,4 +111,4 @@ const mapDispatchToProps = dispatch => ({
   localStorage: bindActionCreators(localStorage, dispatch)
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(About)
+export default connect(mapStateToProps, mapDispatchToProps)(createForm()(About))
