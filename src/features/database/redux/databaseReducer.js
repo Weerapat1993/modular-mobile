449fbb41fc1779bf154427d76b42921fe49dc7ad
Weerapat Thawatchoketawee <weerapat.t@scale360solutions.com>
2018-04-25 16:18:05 +0700

    Add Database github

--- src/features/database/redux/databaseReducer.js
+++ src/features/database/redux/databaseReducer.js
@@ -1,8 +1,10 @@
 import { Reducer, classReducer } from '../../../utils'
 import { FETCH_PURCHASE_LIST } from '../../purchase/redux/purchaseActionTypes'
+import { FETCH_GITHUB_BY_ID } from '../../github/redux/githubActionTypes'
 
 class DatabaseReducer extends Reducer {
   initialState = {
+    github: {},
     purchase: {},
     storage: {},
   }
@@ -18,9 +20,19 @@ class DatabaseReducer extends Reducer {
   getState() {
     const { type, data } = this.action
     switch (type) {
+      case FETCH_GITHUB_BY_ID.SUCCESS:
+        return this.setState({
+          github: {
+            ...this.state.github,
+            ...this.arrayToObject(data, 'id')
+          }
+        })
       case FETCH_PURCHASE_LIST.SUCCESS:
         return this.setState({
-          purchase: this.arrayToObject(data, 'id')
+          purchase: {
+            ...this.state.purchase,
+            ...this.arrayToObject(data, 'id')
+          }
         })
       default:
         return this.state
--- src/features/github/redux/githubReducer.js
+++ src/features/github/redux/githubReducer.js
@@ -1,6 +1,5 @@
 import { FETCH_GITHUB_BY_ID } from './githubActionTypes'
 import { Reducer, classReducer } from '../../../utils'
-import { Github as Model } from '../../../models/Github'
 
 export class GithubReducer extends Reducer {
   initialState = {
@@ -14,7 +13,7 @@ export class GithubReducer extends Reducer {
       case FETCH_GITHUB_BY_ID.REQUEST:
         return this.setStateWithKeyRequest()
       case FETCH_GITHUB_BY_ID.SUCCESS:
-        return this.setStateWithKeySuccess({ data: data.map(item => Model.set(item)) })
+        return this.setStateWithKeySuccess({ data })
       case FETCH_GITHUB_BY_ID.FAILURE:
         return this.setStateWithKeyFailure()
       default:
